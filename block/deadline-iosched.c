2a588c6df3a080c57a4aaf473f6acc6fc4e7e99d
robcore <robpatershuk@gmail.com>
2019-12-13 23:35:26 -0700

    make deadline better for our phones

--- block/deadline-iosched.c
+++ block/deadline-iosched.c
@@ -17,10 +17,10 @@
 /*
  * See Documentation/block/deadline-iosched.txt
  */
-static const int read_expire = 50;  /* max time before a read is submitted. */
+static const int read_expire = 500;  /* max time before a read is submitted. */
 static const int write_expire = 500; /* ditto for writes, these limits are SOFT! */
-static const int writes_starved = 2;    /* max times reads can starve a write */
-static const int fifo_batch = 8;       /* # of sequential requests treated as one
+static const int writes_starved = 0;    /* max times reads can starve a write */
+static const int fifo_batch = 1;       /* # of sequential requests treated as one
 				     by the above parameters. For throughput. */
 
 struct deadline_data {
