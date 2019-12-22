2354a2db8eb255eeaa96f2ac929cd86394c0de90
robcore <robpatershuk@gmail.com>
2019-12-21 17:08:55 -0700

    dont remember what i did but i did it

--- mx-build.sh
+++ mx-build.sh
@@ -66,6 +66,49 @@ export CROSS_COMPILE="/opt/toolchains/arm-cortex_a15-linux-gnueabihf_5.3/bin/arm
 
 env KCONFIG_NOTIMESTAMP=true &>/dev/null
 
+#!/bin/sh
+
+# Asuswrt-Merlin helper functions
+# For use with Postconf scripts (and others)
+
+_quote() {
+	echo $1 | sed 's/[]\/()$*.^|[]/\\&/g'
+}
+
+# This function looks for a string, and inserts a specified string after it inside a given file
+# $1: the line to locate, $2: the line to insert, $3: Config file where to insert
+pc_insert() {
+	local PATTERN;
+	local CONTENT;
+	PATTERN=$(_quote "$1")
+	CONTENT=$(_quote "$2")
+	sed -i "/$PATTERN/a$CONTENT" $3
+}
+
+# This function looks for a string, and replace it with a different string inside a given file
+# $1: the line to locate, $2: the line to replace with, $3: Config file where to insert
+pc_replace() {
+	local PATTERN;
+	local CONTENT;
+	PATTERN=$(_quote "$1")
+	CONTENT=$(_quote "$2")
+	sed -i "s/$PATTERN/$CONTENT/" $3
+}
+
+# This function will append a given string at the end of a given file
+# $1 The line to append at the end, $2: Config file where to append
+pc_append() {
+	echo "$1" >> $2
+}
+
+# This function will delete a line containing a given string inside a given file
+# $1 The line to locate, $2: Config file where to delete
+pc_delete() {
+	local PATTERN;
+	PATTERN=$(_quote "$1")
+	sed -i "/$PATTERN/d" $2
+}
+
 warnandfail() {
 	echo -n "MX ERROR on Line ${BASH_LINENO[0]}"
 	echo "!!!"
