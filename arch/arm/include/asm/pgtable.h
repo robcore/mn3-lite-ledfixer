d8aba7206bd4d84e4aad1ff2920d4da000304567
robcore <robpatershuk@gmail.com>
2019-12-25 14:48:54 -0700

    ridding more tima/sdp/sammy bs, and a reminder to look into arch/arm/include/asm/system_info.h

--- arch/arm/include/asm/pgtable.h
+++ arch/arm/include/asm/pgtable.h
@@ -217,21 +217,18 @@ static inline pte_t *pmd_page_vaddr(pmd_t pmd)
 #define mk_pte(page,prot)	pfn_pte(page_to_pfn(page), prot)
 
 #define pte_clear(mm,addr,ptep)	set_pte_ext(ptep, __pte(0), 0)
-#ifdef	CONFIG_TIMA_RKP_L2_GROUP
-#define timal2group_pte_clear(mm,addr,ptep,tima_l2group_entry_ptr) cpu_v7_timal2group_set_pte_ext(ptep, __pte(0), 0, tima_l2group_entry_ptr)
-#endif /* CONFIG_TIMA_RKP_L2_GROUP */
 
-#define pte_none(pte)          (!pte_val(pte))
-#define pte_present(pte)       (pte_val(pte) & L_PTE_PRESENT)
-#define pte_write(pte)         (!(pte_val(pte) & L_PTE_RDONLY))
-#define pte_dirty(pte)         (pte_val(pte) & L_PTE_DIRTY)
-#define pte_young(pte)         (pte_val(pte) & L_PTE_YOUNG)
-#define pte_exec(pte)          (!(pte_val(pte) & L_PTE_XN))
-#define pte_special(pte)       (0)
+#define pte_none(pte)		(!pte_val(pte))
+#define pte_present(pte)	(pte_val(pte) & L_PTE_PRESENT)
+#define pte_write(pte)		(!(pte_val(pte) & L_PTE_RDONLY))
+#define pte_dirty(pte)		(pte_val(pte) & L_PTE_DIRTY)
+#define pte_young(pte)		(pte_val(pte) & L_PTE_YOUNG)
+#define pte_exec(pte)		(!(pte_val(pte) & L_PTE_XN))
+#define pte_special(pte)	(0)
 
 #define pte_present_user(pte) \
-       ((pte_val(pte) & (L_PTE_PRESENT | L_PTE_USER)) == \
-        (L_PTE_PRESENT | L_PTE_USER))
+	((pte_val(pte) & (L_PTE_PRESENT | L_PTE_USER)) == \
+	 (L_PTE_PRESENT | L_PTE_USER))
 
 #if __LINUX_ARM_ARCH__ < 6
 static inline void __sync_icache_dcache(pte_t pteval)
@@ -253,31 +250,6 @@ static inline void set_pte_at(struct mm_struct *mm, unsigned long addr,
 
 	set_pte_ext(ptep, pteval, ext);
 }
-
-#ifdef CONFIG_TIMA_RKP_L2_GROUP
-static inline void timal2group_set_pte_at(pte_t *ptep, pte_t pteval, 
-			unsigned long tima_l2group_entry_ptr, unsigned long addr,
-			unsigned long *tima_l2group_buffer_index)
-{
-	int ret;
-	if (addr >= TASK_SIZE) {
-		ret = cpu_v7_timal2group_set_pte_ext(ptep, pteval, 0, tima_l2group_entry_ptr);
-		if (ret == 0)
-			(*tima_l2group_buffer_index)++;
-	} else	{
-		__sync_icache_dcache(pteval);
-		ret = cpu_v7_timal2group_set_pte_ext(ptep, pteval, PTE_EXT_NG, tima_l2group_entry_ptr);
-		if (ret == 0)
-			(*tima_l2group_buffer_index)++;
-	}
-}
-static inline void timal2group_set_pte_commit(void *tima_l2group_entry_ptr,
-					unsigned long tima_l2group_entries_count)
-{
-	cpu_v7_timal2group_set_pte_commit(tima_l2group_entry_ptr,
-					 tima_l2group_entries_count);
-}
-#endif /* CONFIG_TIMA_RKP_L2_GROUP */
 
 #define PTE_BIT_FUNC(fn,op) \
 static inline pte_t pte_##fn(pte_t pte) { pte_val(pte) op; return pte; }
@@ -361,7 +333,7 @@ static inline pte_t pte_modify(pte_t pte, pgprot_t newprot)
  * into virtual address `from'
  */
 #define io_remap_pfn_range(vma,from,pfn,size,prot) \
-	remap_pfn_range(vma,from,pfn,size,prot)
+		remap_pfn_range(vma, from, pfn, size, prot)
 
 #define pgtable_cache_init() do { } while (0)
 
