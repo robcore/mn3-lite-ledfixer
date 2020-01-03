b952e9af4db31128782372d217ee008228376762
robcore <robpatershuk@gmail.com>
2020-01-03 03:25:15 -0700

    i forgot how localized loop variables are.  fine then, declare and define them once per loop.

--- patchsplit.sh
+++ patchsplit.sh
@@ -23,36 +23,43 @@ PATCHFOLDER="$(dirname $BIGPATCHNAME)"
 
 rm "$PATCHFOLDER/currentpval" &> /dev/null
 echo -n '0' > "$PATCHFOLDER/currentpval"
-CURRENTPVALFILE="$PATCHFOLDER/currentpval"
-CURRENTPVAL="$(cat $CURRENTPVALFILE)"
 
 dothesplits() {
 
-	local SPLITFILENUM
-	local SPLITFILEFORM
-	local SPLITFILE
-
 	while read -r PLINE
 	do
+		local SPLITFILENUM
+		local SPLITFILEFORM
+		local SPLITFILE
+		local CURRENTPVALFILE
+		local CURRENTPVAL
+		CURRENTPVALFILE="$PATCHFOLDER/currentpval"
+		CURRENTPVAL="$(cat $CURRENTPVALFILE)"
+		echo "$PLINE" | grep -q 'diff --git'
+		if [ "$?" -eq 0 ]
+		then
+			SPLITFILENUM=$((CURRENTPVAL+1))
+			echo -n "$SPLITFILENUM" > "$CURRENTPVALFILE"
+			SPLITFILEFORM=$(printf "%04d\n" $SPLITFILENUM)
+			SPLITFILE="$PATCHFOLDER/$SPLITFILEFORM.patch"
+			echo -ne "                   \r"; \
+			echo -ne "Creating $SPLITFILE\r"; \
+		else
+			SPLITFILEFORM=$(printf "%04d\n" $CURRENTPVAL)
+			SPLITFILE="$PATCHFOLDER/$SPLITFILEFORM.patch"
+			echo -ne "                   \r"; \
+			echo -ne "Creating $SPLITFILE\r"; \
+		fi
+
 		echo "$PLINE" | grep -q 'THISISTHEENDFAKEFAKEFAKEFAKE'
 		if [ "$?" -eq 0 ]
 		then
 			echo -ne "                   \r"; \
 			echo "Finished!"; \
 			break
+		else
+			echo "$PLINE" >> "$SPLITFILE"
 		fi
-
-		echo "$PLINE" | grep -q 'diff --git'
-		if [ "$?" -eq 0 ]
-		then
-			SPLITFILENUM=$((CURRENTPVAL+1))
-			echo -n "$SPLITFILENUM" > "$CURRENTPVALFILE"
-			SPLITFILEFORM=$(printf "%04d\n" "$CURRENTPVAL")
-			SPLITFILE="$PATCHFOLDER/$SPLITFILEFORM.patch"
-			echo -ne "                   \r"; \
-			echo -ne "Creating $SPLITFILE\r"; \
-		fi
-		echo "$PLINE" >> "$SPLITFILE"
 	done < "$BIGPATCHNAME"
 
 }
