917dfaa083190f98f80fbbb4e4be9ade16b2a0c8
robcore <robpatershuk@gmail.com>
2020-01-03 04:53:28 -0700

    add a missing semicolon

--- include/linux/mfd/wcd9xxx/core-resource.h
+++ include/linux/mfd/wcd9xxx/core-resource.h
@@ -132,7 +132,7 @@ int wcd9xxx_bulk_read(struct wcd9xxx_core_resource *,
 int wcd9xxx_bulk_write(struct wcd9xxx_core_resource*,
 					 unsigned short, int, u8*);
 void lock_sound_control(struct wcd9xxx_core_resource *core_res,
-						unsigned int lockval)
+						unsigned int lockval);
 int wcd9xxx_irq_init(struct wcd9xxx_core_resource *);
 void wcd9xxx_irq_exit(struct wcd9xxx_core_resource *);
 int wcd9xxx_core_res_resume(
