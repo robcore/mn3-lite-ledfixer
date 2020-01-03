bbf10e656970ee06919adc3025c7c1905334e89f
robcore <robpatershuk@gmail.com>
2020-01-03 11:08:36 -0700

    make patchinator way more safe and efficient

--- patchinator.sh
+++ patchinator.sh
@@ -21,21 +21,45 @@ trashman() {
 
 }
 
+warnandfail() {
+
+	echo -n "ERROR on Line ${BASH_LINENO[0]}"
+	echo "!!!"
+	local ISTRING
+	ISTRING="$1"
+	if [ -n "$ISTRING" ]
+	then
+		echo "$ISTRING"
+	fi
+	exit 1
+
+}
+
 echo "PATCHINATOR LOG" > "$STATICLOG"
 echo "---------------" >> "$STATICLOG"
 echo >> "$STATICLOG"
 
-for PFILE in /root/113patches/*
-do
-	patch --dry-run -p1 < "$PFILE" > /dev/null 2>&1
-	if [ $? -eq 0 ]
+realpatch() {
+
+	if [ -z "$1" ]
 	then
-	    patch -s -p1 < "$PFILE" > /dev/null 2>&1
-	else
-		echo "$PFILE" >> "$STATICLOG"
-		patch -p1 < "$PFILE" >> "$STATICLOG"
-		echo " " >> "$STATICLOG"
+		warnandfail "Error! No patch file provided!"
 	fi
+
+	IFILE="$1"
+	patch -p1 < "$IFILE"
+
+}
+
+for PFILE in /root/113patches/*.patch
+do
+	if ! patch --dry-run -p1 < "$PFILE" &> /dev/null
+	then
+		echo "$PFILE" >> "$STATICLOG"
+		patch --dry-run -p1 < "$PFILE" >> "$STATICLOG"
+		echo -e "\n" >> "$STATICLOG"
+	fi
+	realpatch "$PFILE" &> /dev/null
 done
 
 trashman &>/dev/null
