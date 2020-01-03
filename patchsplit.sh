cff738f3b0d3bce0c5049edc1307cff2bcf1a590
robcore <robpatershuk@gmail.com>
2020-01-03 02:41:17 -0700

    created my own little patch splitting script because splitpatch is broken and im too unfamiliar with ruby to fix it

--- patchsplit.sh
+++ patchsplit.sh
@@ -1,3 +1,58 @@
 #!/bin/bash
 
-#grepline 'diff --git'
+printusage() {
+
+	echo "Usage:"
+	echo "$0 /path/to/example.patch"
+}
+
+if [ $# -ne 1 ]
+then
+	printusage
+	exit 1
+fi
+
+if [ -z "$1" ]
+then
+	printusage
+	exit 1
+fi
+
+BIGPATCHNAME="$1"
+PATCHFOLDER="$(dirname $BIGPATCHNAME)"
+
+rm "$PATCHFOLDER/currentpval"
+echo -n '0' > "$PATCHFOLDER/currentpval"
+CURRENTPVALFILE="$PATCHFOLDER/currentpval"
+CURRENTPVAL="$(cat $CURRENTPVALFILE)"
+
+dothesplits() {
+
+	while IFS=$'\n' read -r PLINE
+	do
+		local SPLITFILENUM
+		local SPLITFILEFORM
+		local SPLITFILE
+
+		if echo "$PLINE" | grep -q 'THISISTHEENDFAKEFAKEFAKEFAKE'
+		then
+			echo -ne "                   \r"; \
+			echo "Finished!"; \
+			break
+		fi
+
+		if echo "$PLINE" | grep -q 'diff --git'
+		then
+			SPLITFILENUM="$(echo $(expr $(( $CURRENTPVAL + 1 ))))"
+			echo -n "$SPLITFILENUM" > CURRENTPVALFILE
+			SPLITFILEFORM=$(printf "%04d\n" "$CURRENTPVAL")
+			SPLITFILE="$PATCHFOLDER/$SPLITFILEFORM.patch"
+			echo -ne "                   \r"; \
+			echo -ne "Creating $SPLITFILE\r"; \
+		fi
+		echo "$PLINE" >> "$SPLITFILE"
+	done < "$BIGPATCHNAME"
+
+}
+
+dothesplits
