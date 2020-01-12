fed53b24924971b1aad81cae227dc71478e6fe3c
robcore <robpatershuk@gmail.com>
2020-01-12 03:31:50 -0700

    halfway to 3.4.34 or whatever was next i forget its late

--- drivers/base/bus.c
+++ drivers/base/bus.c
@@ -296,9 +296,7 @@ int bus_for_each_dev(struct bus_type *bus, struct device *start,
 	struct device *dev;
 	int error = 0;
 
-	if (!bus)
-		return -EINVAL;
-	if (!bus->p)
+	if (!bus || !bus->p)
 		return -EINVAL;
 
 	klist_iter_init_node(&bus->p->klist_devices, &i,
@@ -332,11 +330,8 @@ struct device *bus_find_device(struct bus_type *bus,
 	struct klist_iter i;
 	struct device *dev;
 
-	if (!bus)
+	if (!bus || !bus->p)
 		return NULL;
-	if (!bus->p)
-		return NULL;
-
 
 	klist_iter_init_node(&bus->p->klist_devices, &i,
 			     (start ? &start->p->knode_bus : NULL));
