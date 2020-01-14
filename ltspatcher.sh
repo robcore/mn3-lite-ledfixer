7df3606430a6edbcad2fa4ef5c9729c2ae698683
robcore <robpatershuk@gmail.com>
2020-01-14 07:29:28 -0700

    fuck it just keep going

--- ltspatcher.sh
+++ ltspatcher.sh
@@ -338,7 +338,7 @@ do
 		else
 			echo "$PATCHLINE reached"
 		fi
-		animatepause
+		pc_delete "$PATCHLINE"
 	elif grep '/drivers/usb/' "$PATCHLINE"
 	then
 		echo "This would patch /drivers/usb/ ignoring!"
@@ -370,7 +370,7 @@ do
 		pc_delete "$PATCHLINE"
 	else
 		printf "%s\n" "Dry run failed!"
-		animatepause
+		pc_delete "$PATCHLINE"
 	fi
 done < "$PATCHFILE"
 fi
