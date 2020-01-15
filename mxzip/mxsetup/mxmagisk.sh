1c2c087841c3fe217b8f8b26bfb636f115e09172
robcore <robpatershuk@gmail.com>
2020-01-14 18:11:51 -0700

    import a little variation of warnandfail for the patcher

--- mxzip/mxsetup/mxmagisk.sh
+++ mxzip/mxsetup/mxmagisk.sh
@@ -13,15 +13,19 @@ ui_print() {
   echo -e "ui_print $1\nui_print" >> /proc/self/fd/$OUTFD
 }
 
-cd /tmp || 	ui_print "mxmagisk ERROR! Failed to switch directories!"
+warnandfail() {
+  ui_print "$1"
+  exit 1
+}
+
+cd /tmp || warnandfail "mxmagisk ERROR! Failed to switch directories!"
 
 if [ ! -f "/tmp/boot_patch.sh" ] || \
 [ ! -f "/tmp/magiskboot" ] || \
 [ ! -f "/tmp/magiskinit" ] || \
 [ ! -f "/tmp/mxmagisk.sh" ] || \
 [ ! -f "/tmp/util_functions.sh" ]; then
-	ui_print "mxmagisk ERROR! Missing files!"
-	exit 1
+  warnandfail "mxmagisk ERROR! Missing files!"
 fi
 
 ui_print "mxmagisk - running magisk boot patch script"
@@ -29,8 +33,7 @@ ui_print "mxmagisk - running magisk boot patch script"
 ./boot_patch.sh "boot.img"
 
 if [ ! -f "new-boot.img" ]; then
-	ui_print "mxmagisk ERROR! Failed to patch boot.img!"
-	exit 1
+  warnandfail "mxmagisk ERROR! Failed to patch boot.img!"
 fi
 
 ui_print "mxmagisk - flashing patched boot.img"
@@ -38,4 +41,3 @@ ui_print "mxmagisk - flashing patched boot.img"
 dd if="new-boot.img" of="/dev/block/platform/msm_sdcc.1/by-name/boot"
 
 ui_print "Finished! Back to the main process!"
-
