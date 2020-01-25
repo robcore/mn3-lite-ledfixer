aa1dc8b5a1856ae95cfcce4208c518c04482e07f
robcore <robpatershuk@gmail.com>
2020-01-25 01:55:22 -0700

    fake selinux on again

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -136,7 +136,7 @@ static ssize_t sel_read_enforce(struct file *filp, char __user *buf,
 	char tmpbuf[TMPBUFLEN];
 	ssize_t length;
 
-	length = scnprintf(tmpbuf, TMPBUFLEN, "%d", 0);
+	length = scnprintf(tmpbuf, TMPBUFLEN, "%u", 1);
 	return simple_read_from_buffer(buf, count, ppos, tmpbuf, length);
 }
 
