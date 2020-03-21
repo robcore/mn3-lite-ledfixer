a05092bfc68cf6c14b9984c430137cec08137c27
robcore <robpatershuk@gmail.com>
2020-03-20 23:38:06 -0600

    added header for 1.65, fixed script to count 0

--- candela_calc.sh
+++ candela_calc.sh
@@ -2,7 +2,7 @@
 
 candela_func() {
 	local bracket="$(qalc -t $1/255)"
-	local exponent="$(qalc -t $bracket^1.65)"
+	local exponent="$(qalc -t $bracket^1.6)"
 	local multone="$(qalc -t 300*$exponent)"
 	local multtwo="$(qalc -t $multone*4194304)"
 	printf "%.0f" "$multtwo"
@@ -11,7 +11,7 @@ candela_func() {
 }
 
 END=255
-for ((i=1;i<=END;i++)); do
+for ((i=0;i<=END;i++)); do
     candela_func $i
 done
 
