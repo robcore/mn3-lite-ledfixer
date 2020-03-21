342614a93a5fa9876872d888e1ecf06a1dbac459
robcore <robpatershuk@gmail.com>
2020-03-20 23:34:18 -0600

    replace <300 with 1.65 candela coeff, and 300 with 1.6.  next commit will replace the curves with their matching gamma curves using a nifty script i wrote

---
+++ candela_calc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+candela_func() {
+	local bracket="$(qalc -t $1/255)"
+	local exponent="$(qalc -t $bracket^1.65)"
+	local multone="$(qalc -t 300*$exponent)"
+	local multtwo="$(qalc -t $multone*4194304)"
+	printf "%.0f" "$multtwo"
+	echo ' ,'
+	#echo "$1 out of 255 = $multtwo"
+}
+
+END=255
+for ((i=1;i<=END;i++)); do
+    candela_func $i
+done
+
+#limit=255
+#for ((i=1;i<=END;++i)); do echo $i; done
+#for i in $(seq 1 $limit)
+#do
+#	CURVAL="$(echo $(( i + 1 )))"
+#	echo "$CURVAL"
+#done
