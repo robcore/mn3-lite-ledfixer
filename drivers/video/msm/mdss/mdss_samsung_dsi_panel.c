b502f2cef1cf1d93577939a2a2f8b66711dad033
robcore <robpatershuk@gmail.com>
2020-04-02 04:12:43 -0600

    revert raw panel command printing it is too much

--- drivers/video/msm/mdss/mdss_samsung_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_samsung_dsi_panel.c
@@ -45,9 +45,9 @@
 #endif
 #if defined(CONFIG_FB_MSM_MIPI_SAMSUNG_OCTA_CMD_FULL_HD_PT_PANEL) \
 	|| defined(CONFIG_FB_MSM_MIPI_SAMSUNG_YOUM_CMD_FULL_HD_PT_PANEL)
-//#define SMART_ACL
+#define SMART_ACL
 #define HBM_RE
-//#define TEMPERATURE_ELVSS_S6E3FA0
+#define TEMPERATURE_ELVSS_S6E3FA0
 #define PARTIAL_UPDATE
 #endif
 #if defined(CONFIG_FB_MSM_MIPI_SAMSUNG_OCTA_CMD_FULL_HD_PT_PANEL)
@@ -2394,7 +2394,7 @@ static int mipi_samsung_disp_send_cmd(
 	struct dsi_cmd_desc *cmd_desc;
 	int cmd_size = 0;
 	int flag = 0;
-#if 1
+#ifdef CMD_DEBUG
 	int i,j;
 #endif
 
@@ -2548,12 +2548,15 @@ static int mipi_samsung_disp_send_cmd(
 		goto unknown_command;
 	}
 
+#ifdef CMD_DEBUG
 	for (i = 0; i < cmd_size; i++)
 	{
 		for (j = 0; j < cmd_desc[i].dchdr.dlen; j++)
 			printk("%x ",cmd_desc[i].payload[j]);
 		printk("\n");
 	}
+#endif
+
 
 #ifdef MDP_RECOVERY
 	if (!mdss_recovery_start)
@@ -2822,10 +2825,12 @@ static int mdss_dsi_panel_dimming_init(struct mdss_panel_data *pdata)
 		/* Read mtp (B5h 19th) for HBM ELVSS OFFSET */
 		mipi_samsung_read_nv_mem(pdata, &nv_hbm_elvss_offset_cmds, hbm_buffer);
 		memcpy(&hbm_etc_cmds_list.cmd_desc[2].payload[1], hbm_buffer, 1);
-
+#if defined(CMD_DEBUG)
+{
 		int i,j;
 
-		for (i = 0; i < hbm_gamma_cmds_list.num_of_cmds; i++) {
+		for (i = 0; i < hbm_gamma_cmds_list.num_of_cmds; i++)
+		{
 			printk("[HBM] hbm_gamma_cmds_list : ");
 
 			for (j = 0; j < hbm_gamma_cmds_list.cmd_desc[i].dchdr.dlen; j++)
@@ -2833,13 +2838,16 @@ static int mdss_dsi_panel_dimming_init(struct mdss_panel_data *pdata)
 			printk("\n");
 		}
 
-		for (i = 0; i < hbm_etc_cmds_list.num_of_cmds; i++) {
+		for (i = 0; i < hbm_etc_cmds_list.num_of_cmds; i++)
+		{
 			printk("[HBM] hbm_etc_cmds_list : ");
 
 			for (j = 0; j < hbm_etc_cmds_list.cmd_desc[i].dchdr.dlen; j++)
 				printk("%02x ",hbm_etc_cmds_list.cmd_desc[i].payload[j]);
 			printk("\n");
 		}
+}
+#endif
 		/* Read mtp (B5h 24th ~ 25th) for Panel Production Day */
 		mipi_samsung_read_nv_mem(pdata, &nv_production_day_cmds, hbm_buffer);
 #endif
