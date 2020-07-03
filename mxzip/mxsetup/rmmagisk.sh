cbf1b4f349228fd84db82b908b688686c8c4a36b
robcore <robpatershuk@gmail.com>
2020-07-02 23:09:29 -0600

    include a repaired platform.xml in update zip

--- mxzip/mxsetup/rmmagisk.sh
+++ mxzip/mxsetup/rmmagisk.sh
@@ -1,3 +1,7 @@
 #!/system/bin/sh
 
 rm -rf /data/magisk_backup*
+chown 0:0 /system/etc/permissions/platform.xml
+chmod 644 /system/etc/permissions/platform.xml
+chown 0:2000 /system/etc/permissions
+chmod 755 /system/etc/permissions
