0442e544f78bc51cbef8f08a727fa5851cb76449
robcore <robpatershuk@gmail.com>
2020-07-06 18:24:33 -0600

    use built in twrp install command to flash magisk directly from our installer (EXPERIMENTAL!! PLEASE WORK)

--- mxzip/mxsetup/magician.sh
+++ mxzip/mxsetup/magician.sh
@@ -5,3 +5,4 @@ chown 0:2000 /system/etc/permissions
 chmod 755 /system/etc/permissions
 chown 0:0 /system/etc/permissions/platform.xml
 chmod 644 /system/etc/permissions/platform.xml
+twrp install /tmp/Magisk-v20.4.zip
