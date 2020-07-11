2488cb1ea9dd9ad25361209796218b909882723f
robcore <robpatershuk@gmail.com>
2020-07-10 19:22:30 -0600

    meh this really doesnt matter

--- drivers/usb/core/message.c
+++ drivers/usb/core/message.c
@@ -1771,9 +1771,9 @@ free_interfaces:
 		goto free_interfaces;
 	}
 
-	dev->actconfig = cp;
 	if (cp)
 		usb_notify_config_device(dev);
+
 	ret = usb_control_msg(dev, usb_sndctrlpipe(dev, 0),
 			      USB_REQ_SET_CONFIGURATION, 0, configuration, 0,
 			      NULL, 0, USB_CTRL_SET_TIMEOUT);
@@ -1781,8 +1781,10 @@ free_interfaces:
 		/* All the old state is gone, so what else can we do?
 		 * The device is probably useless now anyway.
 		 */
-		dev->actconfig = cp = NULL;
+		cp = NULL;
 	}
+
+	dev->actconfig = cp;
 
 	if (!cp) {
 		usb_notify_config_device(dev);
