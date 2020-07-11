a12af6c9667faaa7b65fe7ea65019721190ea202
robcore <robpatershuk@gmail.com>
2020-07-11 14:48:34 -0600

    did some ping magic regarding ipv6

--- net/ipv4/ping.c
+++ net/ipv4/ping.c
@@ -849,17 +849,13 @@ int ping_recvmsg(struct kiocb *iocb, struct sock *sk, struct msghdr *msg,
 	err = -EOPNOTSUPP;
 	if (flags & MSG_OOB)
 		goto out;
-#if IS_ENABLED(CONFIG_IPV6)
-	if (family == AF_INET6 && addr_len)
-			*addr_len = sizeof(*sin6);
-#endif
 
 	if (flags & MSG_ERRQUEUE) {
 		if (family == AF_INET) {
 			return ip_recv_error(sk, msg, len, addr_len);
 #if IS_ENABLED(CONFIG_IPV6)
 		} else if (family == AF_INET6) {
-			return pingv6_ops.ipv6_recv_error(sk, msg, len);
+			return pingv6_ops.ipv6_recv_error(sk, msg, len, addr_len);
 #endif
 		}
 	}
@@ -898,9 +894,8 @@ int ping_recvmsg(struct kiocb *iocb, struct sock *sk, struct msghdr *msg,
 	} else if (family == AF_INET6) {
 		struct ipv6_pinfo *np = inet6_sk(sk);
 		struct ipv6hdr *ip6 = ipv6_hdr(skb);
-		sin6 = (struct sockaddr_in6 *) msg->msg_name;
-
-		if (sin6) {
+		if (msg->msg_name) {
+			sin6 = (struct sockaddr_in6 *) msg->msg_name;
 			sin6->sin6_family = AF_INET6;
 			sin6->sin6_port = 0;
 			sin6->sin6_addr = ip6->saddr;
@@ -913,7 +908,6 @@ int ping_recvmsg(struct kiocb *iocb, struct sock *sk, struct msghdr *msg,
 			sin6->sin6_scope_id = ipv6_iface_scope_id(&sin6->sin6_addr,
 					IP6CB(skb)->iif);
 		}
-
 		if (inet6_sk(sk)->rxopt.all)
 			pingv6_ops.datagram_recv_ctl(sk, msg, skb);
 #endif
