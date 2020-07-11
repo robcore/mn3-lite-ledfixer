8c211343e8e7dd709fc12f0dda6e6c685288a229
robcore <robpatershuk@gmail.com>
2020-07-11 12:18:45 -0600

    3.4.59

--- arch/arm/kernel/perf_event.c
+++ arch/arm/kernel/perf_event.c
@@ -119,7 +119,7 @@ armpmu_map_event(const unsigned (*event_map)[PERF_COUNT_HW_MAX], u64 config)
 	int mapping;
 
 	if (config >= PERF_COUNT_HW_MAX)
-		return -EINVAL;
+		return -ENOENT;
 
 	mapping = (*event_map)[config];
 	return mapping == HW_OP_UNSUPPORTED ? -ENOENT : mapping;
@@ -350,13 +350,8 @@ validate_event(struct pmu_hw_events *hw_events,
 	struct hw_perf_event fake_event = event->hw;
 	struct pmu *leader_pmu = event->group_leader->pmu;
 
-/* Samsung Stock
 	if (is_software_event(event))
 		return 1;
-
-	if (event->pmu != leader_pmu || event->state <= PERF_EVENT_STATE_OFF)
-		return 1;
-*/
 
 	if (event->pmu != leader_pmu || event->state < PERF_EVENT_STATE_OFF)
 		return 1;
