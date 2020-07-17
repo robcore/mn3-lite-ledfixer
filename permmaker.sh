ea74ebb0c19467ea02c2f8641d582d26d463735e
robcore <robpatershuk@gmail.com>
2020-07-16 21:51:24 -0600

    converted ramdisk startup to set permissions regardless of pre-existing folders

--- permmaker.sh
+++ permmaker.sh
@@ -11,11 +11,10 @@ touch "$OUTFILE"
 while IFS= read -r ILINE
 do
 	MKDRCMD=$(echo -n "$ILINE" | cut -z -d ' ' -f1)
-
 	TARG=$(echo -n "$ILINE" | cut -z -d ' ' -f2)
 	MODE=$(echo -n "$ILINE" | cut -z -d ' ' -f3)
 	OWNR=$(echo -n "$ILINE" | cut -z -d ' ' -f4-5)
-	echo "$ILINE" >> "$OUTFILE"
-	echo "$CHMDCMD $MODE $TARG" >> "$OUTFILE"
-	echo "$CHWNCMD $OWNR $TARG" >> "$OUTFILE"
+	echo "    $ILINE" >> "$OUTFILE"
+	echo "    $CHMDCMD $MODE $TARG" >> "$OUTFILE"
+	echo "    $CHWNCMD $OWNR $TARG" >> "$OUTFILE"
 done < "$LISTFILE"
