4e957ae965b04605f6a5681929bad3f203393c89
robcore <robpatershuk@gmail.com>
2020-07-16 21:18:17 -0600

    quick little interpreter script for converting mkdir init commands to mkdir, chmod and chown (for perms fixing folders that already exist)

---
+++ permmaker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+LISTFILE="/root/mn3lite/initlist"
+OUTFILE="/root/mn3lite/initout"
+CHMDCMD=$(echo -n 'chmod')
+CHWNCMD=$(echo -n 'chown')
+
+[ -f "$OUTFILE" ] && rm "$OUTFILE"
+touch "$OUTFILE"
+
+while IFS= read -r ILINE
+do
+	MKDRCMD=$(echo -n "$ILINE" | cut -z -d ' ' -f1)
+
+	TARG=$(echo -n "$ILINE" | cut -z -d ' ' -f2)
+	MODE=$(echo -n "$ILINE" | cut -z -d ' ' -f3)
+	OWNR=$(echo -n "$ILINE" | cut -z -d ' ' -f4-5)
+	echo "$ILINE" >> "$OUTFILE"
+	echo "$CHMDCMD $MODE $TARG" >> "$OUTFILE"
+	echo "$CHWNCMD $OWNR $TARG" >> "$OUTFILE"
+done < "$LISTFILE"
