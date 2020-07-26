1ea8576d395be4b84a56463635b486789ff8f7ce
robcore <robpatershuk@gmail.com>
2020-07-25 22:28:50 -0600

    ASoC-wcd9320-Fix-24-bit-format-for-codec-dai-s

--- sound/soc/codecs/wcd9320.c
+++ sound/soc/codecs/wcd9320.c
@@ -341,7 +341,7 @@ static void set_uhqa_mode(unsigned int enable) {
 #define TAIKO_MCLK_CLK_9P6MHZ 9600000
 
 #define TAIKO_FORMATS_S16_S24_LE (SNDRV_PCM_FMTBIT_S16_LE | \
-			SNDRV_PCM_FORMAT_S24_LE)
+			SNDRV_PCM_FMTBIT_S24_LE)
 
 #define TAIKO_FORMATS (SNDRV_PCM_FMTBIT_S16_LE)
 
