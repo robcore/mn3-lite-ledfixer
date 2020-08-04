6497bd19480befa06e2513990e1288fdec1eee48
robcore <robpatershuk@gmail.com>
2020-08-03 20:07:42 -0600

    add an easy little override for the -6db compander gain reduction when buck mv is set to 1.8

--- sound/soc/codecs/wcd9320.c
+++ sound/soc/codecs/wcd9320.c
@@ -563,6 +563,8 @@ static unsigned int high_perf_mode = 0;
 static unsigned int interpolator_boost = 0;
 static bool hpwidget = false;
 static bool spkwidget = false;
+static unsigned int 6db_gain_lock;
+static unsigned int 6db_gain_boost;
 
 static void update_headphone_gain(void) {
 	if (!hpwidget)
@@ -1158,12 +1160,21 @@ static int taiko_config_compander(struct snd_soc_dapm_widget *w,
 				    TAIKO_A_CDC_COMP0_FS_CFG + (comp * 8),
 				    0x07, rate);
 		/* Set the static gain offset */
-		if (comp == COMPANDER_1
+		if (6db_gain_lock && comp == COMPANDER_1) {
+			if (6db_gain_boost)
+				snd_soc_update_bits(codec,
+						TAIKO_A_CDC_COMP0_B4_CTL + (comp * 8),
+						0x80, 0x00);
+			else
+				snd_soc_update_bits(codec,
+						TAIKO_A_CDC_COMP0_B4_CTL + (comp * 8),
+						0x80, 0x80);
+		} else if (!6db_gain_lock && comp == COMPANDER_1
 			&& buck_mv == WCD9XXX_CDC_BUCK_MV_1P8) {
 			snd_soc_update_bits(codec,
 					TAIKO_A_CDC_COMP0_B4_CTL + (comp * 8),
 					0x80, 0x80);
-		} else {
+		} else if (!6db_gain_lock) {
 			snd_soc_update_bits(codec,
 					TAIKO_A_CDC_COMP0_B4_CTL + (comp * 8),
 					0x80, 0x00);
@@ -7660,6 +7671,46 @@ static ssize_t hph_pa_enabled_store(struct kobject *kobj,
 	return count;
 }
 
+static ssize_t 6db_gain_lock_show(struct kobject *kobj,
+		struct kobj_attribute *attr, char *buf) {
+	return sprintf(buf, "%u\n", 6db_gain_lock);
+}
+
+static ssize_t 6db_gain_lock_store(struct kobject *kobj,
+			   struct kobj_attribute *attr, const char *buf, size_t count) {
+	int uval;
+
+	sscanf(buf, "%d", &uval);
+
+	if (uval < 0)
+		uval = 0;
+	if (uval > 1)
+		uval = 1;
+
+	6db_gain_lock = uval;
+	return count;
+}
+
+static ssize_t 6db_gain_boost_show(struct kobject *kobj,
+		struct kobj_attribute *attr, char *buf) {
+	return sprintf(buf, "%u\n", 6db_gain_boost);
+}
+
+static ssize_t 6db_gain_boost_store(struct kobject *kobj,
+			   struct kobj_attribute *attr, const char *buf, size_t count) {
+	int uval;
+
+	sscanf(buf, "%d", &uval);
+
+	if (uval < 0)
+		uval = 0;
+	if (uval > 1)
+		uval = 1;
+
+	6db_gain_boost = uval;
+	return count;
+}
+
 
 static struct kobj_attribute headphone_gain_attribute =
 	__ATTR(headphone_gain, 0644,
@@ -7696,6 +7747,16 @@ static struct kobj_attribute hph_pa_enabled_attribute =
 		hph_pa_enabled_show,
 		hph_pa_enabled_store);
 
+static struct kobj_attribute 6db_gain_lock_attribute =
+	__ATTR(6db_gain_lock, 0644,
+		6db_gain_lock_show,
+		6db_gain_lock_store);
+
+static struct kobj_attribute 6db_gain_boost_attribute =
+	__ATTR(6db_gain_boost, 0644,
+		6db_gain_boost_show,
+		6db_gain_boost_store);
+
 static struct attribute *sound_control_attrs[] = {
 		&headphone_gain_attribute.attr,
 		&hph_poweramp_gain_attribute.attr,
@@ -7704,6 +7765,8 @@ static struct attribute *sound_control_attrs[] = {
 		&high_perf_mode_attribute.attr,
 		&interpolator_boost_attribute.attr,
 		&hph_pa_enabled_attribute.attr,
+		&6db_gain_lock_attribute.attr,
+		&6db_gain_boost_attribute.attr,
 		NULL,
 };
 
