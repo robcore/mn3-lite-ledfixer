f9569061fe9cfa269c6d95cf49f7d4f90e76d255
robcore <robpatershuk@gmail.com>
2020-08-30 19:18:02 -0600

    restore wavegen to previous settings

--- sound/soc/codecs/wcd9320.c
+++ sound/soc/codecs/wcd9320.c
@@ -1291,11 +1291,11 @@ static int taiko_set_compander(struct snd_kcontrol *kcontrol,
 	int value = ucontrol->value.integer.value[0];
 
 	if (hph_pa_enabled && comp == COMPANDER_1) {
+		/* Wavegen to 20 msec */
 		taiko->comp_enabled[comp] = 0;
-   		/* Wavegen to 5 msec */
-   		snd_soc_write(codec, TAIKO_A_RX_HPH_CNP_WG_CTL, 0xDA);
-   		snd_soc_write(codec, TAIKO_A_RX_HPH_CNP_WG_TIME, 0x15);
-   		snd_soc_write(codec, TAIKO_A_RX_HPH_BIAS_WG_OCP, 0x2A);
+		snd_soc_write(codec, TAIKO_A_RX_HPH_CNP_WG_CTL, 0xDB);
+		snd_soc_write(codec, TAIKO_A_RX_HPH_CNP_WG_TIME, 0x58);
+		snd_soc_write(codec, TAIKO_A_RX_HPH_BIAS_WG_OCP, 0x1A);
 
 		/* Disable CHOPPER block */
 		snd_soc_update_bits(codec,
