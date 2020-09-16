af0cf34c744160d334ce0eb8279aa7a8d1f7aa1e
robcore <robpatershuk@gmail.com>
2020-09-16 01:18:32 -0600

    reverted

--- security/selinux/include/avc.h
+++ security/selinux/include/avc.h
@@ -19,11 +19,7 @@
 #include "av_permissions.h"
 #include "security.h"
 
-#ifdef CONFIG_SECURITY_SELINUX_DEVELOP
 extern int selinux_enforcing;
-#else
-#define selinux_enforcing CONFIG_SECURITY_SELINUX_ENFORCING
-#endif
 
 /*
  * An entry in the AVC.
@@ -83,8 +79,7 @@ int avc_audit(u32 ssid, u32 tsid,
 	       int result,
 	      struct common_audit_data *a, unsigned flags);
 
-#define AVC_STRICT 1 /* Ignore permissive mode. */
-#define AVC_EXTENDED_PERMS 2	/* update extended permissions */
+#define AVC_STRICT 0 /* Ignore permissive mode. */
 int avc_has_perm_noaudit(u32 ssid, u32 tsid,
 			 u16 tclass, u32 requested,
 			 unsigned flags,
@@ -102,9 +97,6 @@ static inline int avc_has_perm(u32 ssid, u32 tsid,
 	return avc_has_perm_flags(ssid, tsid, tclass, requested, auditdata, 0);
 }
 
-int avc_has_extended_perms(u32 ssid, u32 tsid, u16 tclass, u32 requested,
-		u8 driver, u8 perm, struct common_audit_data *ad);
-
 u32 avc_policy_seqno(void);
 
 #define AVC_CALLBACK_GRANT		1
@@ -115,7 +107,6 @@ u32 avc_policy_seqno(void);
 #define AVC_CALLBACK_AUDITALLOW_DISABLE	32
 #define AVC_CALLBACK_AUDITDENY_ENABLE	64
 #define AVC_CALLBACK_AUDITDENY_DISABLE	128
-#define AVC_CALLBACK_ADD_XPERMS		256
 
 int avc_add_callback(int (*callback)(u32 event, u32 ssid, u32 tsid,
 				     u16 tclass, u32 perms,
