63e0c333902c040f01a6e7622e7281d190faa0e0
robcore <robpatershuk@gmail.com>
2021-02-03 19:30:41 -0700

    remove remaining knox, all the carrier bloat and obsolete google products i can without breaking things too much (hopefully), and reinstall synapse to system/priv-app

--- mxzip/mxsetup/dosetup.sh
+++ mxzip/mxsetup/dosetup.sh
@@ -21,3 +21,60 @@ if [ -f "/data/dalvik-cache/profiles/com.topjohnwu.magisk" ]
 then
     rm "/data/dalvik-cache/profiles/com.topjohnwu.magisk"
 fi
+
+chmod 755 /system/priv-app/synapse
+chown 0:0 /system/priv-app/synapse
+
+chmod 644 /system/priv-app/synapse/synapse.apk
+chown 0:0  /system/priv-app/synapse/synapse.apk
+
+rm -rf /system/app/SecurityLogAgent
+rm -rf /system/priv-app/DiagMonAgent
+rm -rf /system/priv-app/FotaClient
+rm -rf /system/app/LocalFOTA
+rm -rf /system/app/SecKidsModeInstaller
+rm -rf /system/priv-app/SPDClient
+rm -rf /system/container/*
+rm -rf /system/app/Bloomberg
+rm -rf /system/app/ChatON_MARKET_Canada
+rm -rf /system/app/Hangouts
+rm -rf /system/app/Peel_L
+rm -rf /system/app/PlusOne
+rm -rf /system/app/BellMobileTVStub_New_Icon
+rm -rf /system/app/BellMobileTVStub_New_Icon
+rm -rf /system/app/BellWallet_1.0_stub_new_icon
+rm -rf /system/app/Fido-Ringtones
+rm -rf /system/app/Fido-Shop
+rm -rf /system/app/FidoAppZone_v1.0.25.2-ZA
+rm -rf /system/app/Generic_citytv-stub-signed-aligned
+rm -rf /system/app/Koodo_SelfServe_Virtual_PreLoad_Signed
+rm -rf /system/app/MyAccountStub_v2.0
+rm -rf /system/app/MyAccount_v3.2.1
+rm -rf /system/app/MyBellStub
+rm -rf /system/app/My_Account-android-fido-prod-ZA
+rm -rf /system/app/BellMobileTVStub_New_Icon
+rm -rf /system/app/BellWallet_1.0_stub_new_icon
+rm -rf /system/app/Fido-Ringtones
+rm -rf /system/app/Fido-Shop
+rm -rf /system/app/FidoAppZone_v1.0.25.2-ZA
+rm -rf /system/app/Generic_citytv-stub-signed-aligned
+rm -rf /system/app/Koodo_SelfServe_Virtual_PreLoad_Signed
+rm -rf /system/app/MyAccountStub_v2.0
+rm -rf /system/app/MyAccount_v3.2.1
+rm -rf /system/app/MyBellStub
+rm -rf /system/app/My_Account-android-fido-prod-ZA
+rm -rf /system/app/Navigator_Stub
+rm -rf /system/app/Push_To_Talk
+rm -rf /system/app/Rogers-Navigator
+rm -rf /system/app/Rogers-OneNumber
+rm -rf /system/app/Rogers-Ringtones
+rm -rf /system/app/Rogers-Shop
+rm -rf /system/app/RogersAppZone_v1.0.25.2-ZA
+rm -rf /system/app/Samsung_Note_3_KitKat_AnyplaceTV
+rm -rf /system/app/Samsung_Note_3_KitKat_Fido_Navigator
+rm -rf /system/app/TELUSFeaturedApps_v1.0_Signed_Prod_App
+rm -rf /system/app/Telus_MyAccount_Virtual_PreLoad_Singed_May10
+rm -rf /system/app/TopHDGames_HUB_SamsungH_EN_Rogers_CA_100
+rm -rf /system/app/TopHDGames_HUB_SamsungH_EN_Rogers_CA_100/lib
+rm -rf /system/app/TopHDGames_HUB_SamsungH_EN_Rogers_CA_100/lib/arm
+rm -rf /system/app/videotron-android
