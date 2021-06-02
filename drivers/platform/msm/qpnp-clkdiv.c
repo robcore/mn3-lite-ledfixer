8ae34681e3e854c1de8d7c00199326c4c3e0cf76
robcore <robpatershuk@gmail.com>
2021-06-02 17:43:20 -0600

    doing some thangs

--- drivers/platform/msm/qpnp-clkdiv.c
+++ drivers/platform/msm/qpnp-clkdiv.c
@@ -202,7 +202,7 @@ static int __devinit qpnp_clkdiv_probe(struct spmi_device *spmi)
 	if (!res) {
 		dev_err(&spmi->dev, "%s: unable to get device reg resource\n",
 					__func__);
-		return -EINVAL;
+		return -ENOMEM;
 	}
 
 	q_clkdiv->slave = spmi->sid;
