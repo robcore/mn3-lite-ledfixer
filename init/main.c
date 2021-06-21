3008ca2897ce5ea7a40f0754ddc46924d8c7c827
robcore <robpatershuk@gmail.com>
2021-06-21 15:06:13 -0600

    revert backport of high res timers from lineage

--- init/main.c
+++ init/main.c
@@ -69,7 +69,6 @@
 #include <linux/slab.h>
 #include <linux/perf_event.h>
 #include <linux/random.h>
-#include <linux/sched_clock.h>
 #include <linux/s_funcs.h>
 
 #include <asm/io.h>
@@ -620,7 +619,6 @@ asmlinkage void __init start_kernel(void)
 	softirq_init();
 	timekeeping_init();
 	time_init();
-	sched_clock_postinit();
 	profile_init();
 	call_function_init();
 	if (!irqs_disabled())
