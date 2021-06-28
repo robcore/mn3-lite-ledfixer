6fa157e1b94d1cabafaaca53c6301443c74e13b1
robcore <robpatershuk@gmail.com>
2021-06-28 09:26:58 -0600

    try to debug qpnp

--- mxbuild.sh
+++ mxbuild.sh
@@ -633,7 +633,7 @@ build_boot_img() {
 	cat "$FIXUP.bin" "$NEWZMG" > "$FZMG" || warnandfail "Can't build fixup"
 
     [ -f "$MXZMG" ] && rm "$MXZMG"
-    cp "$FZMG" "$MXZMG" || warnandfail "Failed to copy $NEWZMG to $MXZMG!"
+    cp "$FZMG" "$MXZMG" || warnandfail "Failed to copy $MXZMG to $MXZMG!"
     chmod 644 "$MXZMG"
 
     cd "$MXRD" || warnandfail "Failed to cd into $MXRD!"
