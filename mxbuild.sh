a86eeaf56d814a38a3068048f76c34195bbe8303
robcore <robpatershuk@gmail.com>
2021-07-04 16:20:10 -0600

    fix date again

--- mxbuild.sh
+++ mxbuild.sh
@@ -267,45 +267,45 @@ _quote() {
 
 # This function looks for a string, and inserts a specified string after it inside a given file
 # $1: the line to locate, $2: the line to insert, $3: Config file where to insert
-pc_insert() {
-
-	local PATTERN;
-	local CONTENT;
-	PATTERN=$(_quote "$1")
-	CONTENT=$(_quote "$2")
-	sed -i "/$PATTERN/a$CONTENT" $3
-
-}
+#pc_insert() {
+#
+#	local PATTERN;
+#	local CONTENT;
+#	PATTERN=$(_quote "$1")
+#	CONTENT=$(_quote "$2")
+#	sed -i "/$PATTERN/a$CONTENT" $3
+#
+#}
 
 # This function looks for a string, and replace it with a different string inside a given file
 # $1: the line to locate, $2: the line to replace with, $3: Config file where to insert
-pc_replace() {
-
-	local PATTERN;
-	local CONTENT;
-	PATTERN=$(_quote "$1")
-	CONTENT=$(_quote "$2")
-	sed -i "s/$PATTERN/$CONTENT/" $3
-
-}
+#pc_replace() {
+#
+#	local PATTERN;
+#	local CONTENT;
+#	PATTERN=$(_quote "$1")
+#	CONTENT=$(_quote "$2")
+#	sed -i "s/$PATTERN/$CONTENT/" $3
+#
+#}
 
 # This function will append a given string at the end of a given file
 # $1 The line to append at the end, $2: Config file where to append
-pc_append() {
-
-	echo "$1" >> $2
-
-}
+#pc_append() {
+#
+#	echo "$1" >> $2
+#
+#}
 
 # This function will delete a line containing a given string inside a given file
 # $1 The line to locate, $2: Config file where to delete
-pc_delete() {
-
-	local PATTERN;
-	PATTERN=$(_quote "$1")
-	sed -i "/$PATTERN/d" $2
-
-}
+#pc_delete() {
+#
+#	local PATTERN;
+#	PATTERN=$(_quote "$1")
+#	sed -i "/$PATTERN/d" $2
+#
+#}
 
 if [ "$1" != "-nc" ] && [ "$1" != "--newconfig" ]
 then
